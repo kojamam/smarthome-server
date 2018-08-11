76d1bb921cd9110d50e4a7f46689f408591e2568
Koji Yamamoto <kyamuise@gmail.com>
2018-08-11 12:25:28 +0900

    websocket

--- app.js
+++ app.js
@@ -1,45 +1,91 @@
 `use strict`
 
-const createError = require('http-errors');
+/**
+ * Dependency
+ */
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv');
+let WebSocketServer = require('websocket').server;
 
-
-const homeRouter = require('./routes/home');
-
+/**
+ * Initialization
+ */
 dotenv.config();
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+/**
+ * express configuuration
+ */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routing
-app.use('/home', homeRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+/**
+ * express routing
+ */
+app.post('/home', function (req, res, next) {
+  let msg;
+  sendToClient(JSON.stringify(req.body));
+  if (req.body.token === process.env.ACCESS_TOKEN) {
+    res.send({ "result": "accepted" });
+  } else {
+    res.status(400).send({ "result": "invalid token" });
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+/**
+ * Start server
+ */
+let port = process.env.PORT || 5000;
+let server = app.listen(port, () => {
+  console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
-module.exports = app;
+/**
+ * WebSocket
+ */
+wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  //TODO ここで接続元を見る
+  console.log(origin);
+  return true;
+}
+
+let connection = null;
+
+wsServer.on('request', function (request) {
+  if (!originIsAllowed(request.origin)) {
+    request.reject();
+    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
+
+  connection = request.accept('khp', request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+  connection.on('message', function (message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ' + message.utf8Data);
+      connection.sendUTF(message.utf8Data);
+    } else if (message.type === 'binary') {
+      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+      connection.sendBytes(message.binaryData);
+    }
+  });
+  connection.on('close', function (reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    connection = null;
+  });
+});
+
+function sendToClient(payload) {
+  connection.sendUTF(payload);
+}
--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
+const ws = require('../lib/ws');
 
 /* smart home api */
 router.post('/', function (req, res, next) {
     let msg;
+    ws(JSON.stringify(req.body));
     if (req.body.token === process.env.ACCESS_TOKEN) {
         res.send({ "result": "accepted" });
     } else {
