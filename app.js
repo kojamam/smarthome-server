255e8e54572b564f797a4d70a9da58c0178141be
Koji Yamamoto <kyamuise@gmail.com>
2019-03-24 15:15:22 +0900

    remove unused packages

--- app.js
+++ app.js
@@ -5,8 +5,6 @@
  */
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const dotenv = require('dotenv');
 let WebSocketServer = require('websocket').server;
 
@@ -19,10 +17,8 @@ const app = express();
 /**
  * express configuuration
  */
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 /**
